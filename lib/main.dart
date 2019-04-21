89340ea7726ac0cfdc643632f1c460ce5d301c13
art1415926535 <art1415926535@yandex.ru>
2019-04-21 19:47:37 +0300

    frontend ready.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,33 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'RAID 50'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,111 +29,366 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   List storage = [
     [
       {
-        'data': ['a11', 'b11', Null],
-        'size': 100, // bytes
+        'data': ['a11', 'b11', null],
+        'size': 1024, // bytes
         'avaliable': 3, // bytes
       }, // Disk 1.
       {
-        'data': ['a12', Null, 'c11'],
-        'size': 100, // bytes
+        'data': ['a12', null, 'c11'],
+        'size': 1024, // bytes
         'avaliable': 3, // bytes
       }, // Disk 2.
       {
-        'data': [Null, 'b12', 'c12'],
-        'size': 100, // bytes
+        'data': [null, 'b12', 'c12'],
+        'size': 1024, // bytes
         'avaliable': 3, // bytes
       }, // Disk 3.
     ], // RAID 5.
     [
       {
-        'data': [Null, 'b21', 'c21'],
-        'size': 100, // bytes
+        'data': [null, 'b21', 'c21'],
+        'size': 1024, // bytes
         'avaliable': 3, // bytes
       }, // Disk 4.
       {
-        'data': ['a21', Null, 'c22'],
-        'size': 100, // bytes
+        'data': ['a21', null, 'c22'],
+        'size': 1024, // bytes
         'avaliable': 3, // bytes
       }, // Disk 5.
       {
-        'data': ['a22', 'b22', Null],
-        'size': 100, // bytes
+        'data': ['a22', 'b22', null],
+        'size': 1024, // bytes
         'avaliable': 3, // bytes
       }, // Disk 3.
     ], // RAID 5.
   ]; // RAID 0.
 
-  void _incrementCounter() {
+  int pressedRow;
+
+  void _addDisksDialog(BuildContext context) async {
+    int diskCount;
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: const Text('Выберите количество дисков'),
+            children: List<Widget>.generate(5, (int i) {
+              return SimpleDialogOption(
+                onPressed: () {
+                  diskCount = (i + 1) * 2;
+                  Navigator.pop(context);
+                },
+                child: Text('+ ${(i + 1) * 2}'),
+              );
+            }),
+          );
+        });
+    if (diskCount == null) return;
+
+    _errorDialog(context, 'Не были добавлены диски: $diskCount шт');
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      storage = storage;
+    });
+  }
+
+  void _updateDiskSize(BuildContext context) async {
+    int diskSize;
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: const Text('Выберите размер дисков'),
+            children: List<Widget>.generate(10, (int i) {
+              return SimpleDialogOption(
+                onPressed: () {
+                  diskSize = pow(2, i + 2);
+                  Navigator.pop(context);
+                },
+                child: Text('${pow(2, i + 2)} байт'),
+              );
+            }),
+          );
+        });
+    if (diskSize == null) return;
+
+    _errorDialog(context, 'Размер дисков не был изменен до $diskSize байт');
+
+    setState(() {
+      storage = storage;
+    });
+  }
+
+  void _addDataDialog(BuildContext context) async {
+    String data;
+    await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Введите данные для сохранения в RAID'),
+          content: new Row(
+            children: <Widget>[
+              new Expanded(
+                  child: new TextField(
+                autofocus: true,
+                decoration: new InputDecoration(
+                    labelText: 'Данные', hintText: 'Строка данных'),
+                onChanged: (value) {
+                  data = value;
+                },
+              ))
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Добавить данные'),
+              onPressed: () async {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+    if (data == null || data == '') return;
+
+    await _errorDialog(context, 'Не были добавлены данные: "$data"');
+
+    setState(() {
+      storage = storage;
+    });
+  }
+
+  Future<String> _errorDialog(BuildContext context, String text) async {
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(title: Text('Ошибка'), content: Text(text));
+      },
+    );
+  }
+
+  void selectData(int dataIndex) {
+    setState(() {
+      pressedRow = dataIndex;
+    });
+  }
+
+  void deleteData(int dataIndex) {
+    setState(() {
+      storage = storage;
+      _errorDialog(context, 'Удаление данных №$dataIndex не выполнено');
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
-        children: storage
-            .map((raid5) => Container(
-                child: Column(
-                    children: <Widget>[
-                      Text(
-                        'RAID 0',
-                        style: TextStyle(
-                            color: Theme.of(context).accentColor,
-                            fontSize: 20,
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: storage.length,
+                itemBuilder: (BuildContext ctx, int raid5Index) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'RAID 5 №${raid5Index + 1}',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Theme.of(ctx).accentColor,
+                          ),
                         ),
-                      )
-                    ] + raid5
-                        .map<Widget>(
-                            (disk) => Card(
-                              child: Column(
-                                children: <Widget>[
-                                  Text(
-                                    disk['data'].toString(),
-                                    style: Theme.of(context).textTheme.display1,
-                                  ),
-                                  Text(
-                                    'Size: ${disk["Size"].toString()}',
-                                  ),
-                                  Text(
-                                    'Avaliable: ${disk["Avaliable"].toString()}',
-                                  ),
-                                ],
-                              ),
-                            )
-                        )
-                        .toList()
-                )
-        )
-        )
-            .toList(),
+                        Container(
+                            height: 250,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              itemCount: storage[raid5Index].length,
+                              itemBuilder: (BuildContext ctx, int diskIndex) {
+                                return new DiskWidget(
+                                  storage: storage,
+                                  raid5Index: raid5Index,
+                                  diskIndex: diskIndex,
+                                  parent: this,
+                                );
+                              },
+                            )),
+                      ],
+                    ),
+                  );
+                }),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        curve: Curves.bounceIn,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        tooltip: 'Speed Dial',
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 8.0,
+        shape: CircleBorder(),
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.add),
+              backgroundColor: Colors.blueAccent,
+              label: 'Добавить диски',
+              onTap: () => {_addDisksDialog(context)}
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.photo_size_select_small),
+            backgroundColor: Colors.blueAccent,
+            label: 'Установить размер дисков',
+            onTap: () => {_updateDiskSize(context)},
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.keyboard),
+            backgroundColor: Colors.blueAccent,
+            label: 'Добавить данные',
+            onTap: () => {_addDataDialog(context)},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DiskWidget extends StatelessWidget {
+  const DiskWidget({
+    Key key,
+    @required this.storage,
+    @required this.raid5Index,
+    @required this.diskIndex,
+    @required this.parent,
+  }) : super(key: key);
+
+  final List storage;
+  final int raid5Index;
+  final int diskIndex;
+  final _MyHomePageState parent;
+
+  final Color parityColor = Colors.green;
+  final Color selectedRowColor = const Color.fromRGBO(0, 150, 255, .3);
+  final Color selectedRowParityColor = const Color.fromRGBO(0, 255, 150, .3);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Диск ${raid5Index * storage[0].length + diskIndex + 1}',
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Row(
+              children: <Widget>[
+                Text('Размер диска: '),
+                Text(
+                  '${storage[raid5Index][diskIndex]['size']} Б',
+                  style: Theme.of(context).textTheme.body2,
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Text('Доступно: '),
+                Text(
+                  '${storage[raid5Index][diskIndex]['avaliable']} Б',
+                  style: Theme.of(context).textTheme.body2,
+                ),
+              ],
+            ),
+            Divider(),
+            Text(
+              'Данные',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+            Expanded(
+              child: Container(
+                width: 150,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: storage[raid5Index][diskIndex]['data'].length,
+                    itemBuilder: (BuildContext ctx, int dataIndex) {
+                      var data =
+                          storage[raid5Index][diskIndex]['data'][dataIndex];
+                      Text info;
+                      Icon icon;
+                      if (data != null) {
+                        icon = Icon(
+                          Icons.insert_drive_file,
+                          color: Colors.grey,
+                        );
+                        info = Text(data);
+                      } else {
+                        icon = Icon(
+                          Icons.local_parking,
+                          color: parityColor,
+                        );
+                        var parityOfData = storage[raid5Index]
+                            .map((d) => {
+                                  dataIndex < d['data'].length
+                                      ? d['data'][dataIndex]
+                                      : '<error>'
+                                })
+                            .reduce((m1, m2) => m1..addAll(m2))
+                            .where((f) => f != null)
+                            .join(', ');
+                        info = Text(
+                          '// ' + parityOfData,
+                          style: TextStyle(color: parityColor),
+                        );
+                      }
+                      return InkWell(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 3.0, bottom: 3.0, right: 3.0),
+                          child: Container(
+                            child: Row(
+                              children: <Widget>[
+                                Padding(
+                                    padding: const EdgeInsets.only(right: 6.0),
+                                    child: icon),
+                                Flexible(
+                                  child: info,
+                                ),
+                              ],
+                            ),
+                            decoration: BoxDecoration(
+                              color: parent.pressedRow == dataIndex
+                                  ? (data != null
+                                      ? selectedRowColor
+                                      : selectedRowParityColor)
+                                  : Colors.white,
+                            ),
+                          ),
+                        ),
+                        onTap: () => {parent.selectData(dataIndex)},
+                        onDoubleTap: () => {parent.deleteData(dataIndex)},
+                      );
+                    }),
+              ),
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+        ),
+      ),
     );
   }
 }
